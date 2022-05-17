7cc5b0b536ee411fda644d501c6a4627bb2f37a7
sanyambhatia01 <sanyam.bhatia@gokloud.io>
2022-05-17 23:14:37 +0530

    added

--- Program1.cs
+++ Program1.cs
@@ -1,47 +1,55 @@
-using System;     
-class Test    
-{    
-   static void Main(string[] arg)    
-   {    
-      Console.WriteLine(checkPassward("ghb!2#@fc",5));
-    
-   }    
-   static bool checkPassward(string passWord,int min)
-   {
-       bool Num = false;
-       bool Upp = false;
-       bool Low = false;
-       bool Spec = false;
-       char currentChar;
-      if(!(passWard.length >= min))
-      {
-          return false;
-      }
-      
-       for(int i=0;i< passWard.length;i++)
-       {
-           currentChar = passWord[i];
-           if (char.IsDigit(currentChar))
-           {
-               Num = true;
-           }
-           else if(char.IsUpper(currentChar))
-           {
-               Upp = true;
-           }
-           else if(char.IsLower(currentChar))
-           {
-               Low = true;
-           }
-           else if(!char.IsLetterOrDigit(currentChar))
-           {
-               Spec = true;
-           }
-           if(Num && Upp && Low && Spec)
-           {
-               return true;
-           }
-       }
-       return false;
-   }
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordVerifier
+{
+    public class Program
+    {
+        static void Main(string[] arg)
+        {
+            Console.WriteLine(checkPassword("Abc@123dcjdc!@#$", 10));
+            Console.ReadLine();
+        }
+        static bool checkPassword(string input, int minimum)
+        {
+            bool num = false;
+            bool upp = false;
+            bool low = false;
+            bool spec = false;
+            char currentChar;
+            if (!(input.Length >= minimum))
+            {
+                return false;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                currentChar = input[i];
+                if (char.IsDigit(currentChar))
+                {
+                    num = true;
+                }
+                else if (char.IsUpper(currentChar))
+                {
+                    upp = true;
+                }
+                else if (char.IsLower(currentChar))
+                {
+                    low = true;
+                }
+                else if (!char.IsLetterOrDigit(currentChar))
+                {
+                    spec = true;
+                }
+                if (num && upp && low && spec)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 }
